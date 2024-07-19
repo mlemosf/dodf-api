f117b1338609e2bc0f3da65c7e6fcf7f7ed8a4b6
mlemosf <matheuslemosf@protonmail.com>
2024-07-19 18:49:12 +0000

    fix: Volume do banco de dados

--- utils/db.py
+++ utils/db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = 'sqlite:///./dodf.db'
+DB_URL = 'sqlite:///./db/dodf.db'
 engine = create_engine(
     DB_URL, connect_args={'check_same_thread': False}
 )
